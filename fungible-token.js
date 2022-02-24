1f618a3d474230b57cd0c6a7229a0003aa63c366
Mauricio <mauriciofigueiredo@knights.ucf.edu>
2022-02-24 16:48:38 -0500

    Added fungible token creation process

---
+++ fungible-token.js
@@ -0,0 +1,134 @@
+const xrpl = require("xrpl")
+
+// Configure issuer (cold address) settings
+const cold_settings_tx = {
+    "TransactionType": "AccountSet",
+    "Account": cold_wallet.address,
+    "TransferRate": 0,
+    "TickSize": 5,
+    "Domain": "6578616D706C652E636F6D", // "example.com"
+    "SetFlag": xrpl.AccountSetAsfFlags.asfDefaultRipple,
+    // Using tf flags, we can enable more flags in one transaction
+    "Flags": (xrpl.AccountSetTfFlags.tfDisallowXRP | xrpl.AccountSetTfFlags.tfRequireDestTag)
+}
+
+// Configure receiver (hot address) settings
+const hot_settings_tx = {
+    "TransactionType": "AccountSet",
+    "Account": hot_wallet.address,
+    "Domain": "6578616D706C652E636F6D", // "example.com"
+    // enable Require Auth so we can't use trust lines that users
+    // make to the hot address, even by accident:
+    "SetFlag": xrpl.AccountSetAsfFlags.asfRequireAuth,
+    "Flags": (xrpl.AccountSetTfFlags.tfDisallowXRP | xrpl.AccountSetTfFlags.tfRequireDestTag)
+}
+
+const currency_code = "FOO"
+const trust_set_tx = {
+    "TransactionType": "TrustSet",
+    "Account": hot_wallet.address,
+    "LimitAmount": {
+        "currency": currency_code,
+        "issuer": cold_wallet.address,
+        "value": "10000000000" // Large limit, arbitrarily chosen
+    }
+}
+
+const issue_quantity = "3000"
+const send_token_tx = {
+    "TransactionType": "Payment",
+    "Account": cold_wallet.address,
+    "Amount": {
+        "currency": currency_code,
+        "value": issue_quantity,
+        "issuer": cold_wallet.address
+    },
+    "Destination": hot_wallet.address,
+    "DestinationTag": 1 // Needed since we enabled Require Destination Tags
+                        // on the hot account earlier.
+}
+
+// Wrap code in an async function so we can use await
+async function main() {
+
+    // Define the network client
+    const client = new xrpl.Client("wss://s.altnet.rippletest.net:51233")
+    await client.connect()
+
+    // Configuring cold address settings by sending an ACCOUNT SET transaction
+    // to the XRP network
+    const cst_prepared = await client.autofill(cold_settings_tx)
+    const cst_signed = cold_wallet.sign(cst_prepared)
+
+    console.log("Sending cold address AccountSet transaction...")
+    const cst_result = await client.submitAndWait(cst_signed.tx_blob)
+
+    if (cst_result.result.meta.TransactionResult == "tesSUCCESS") {
+        console.log(`Transaction succeeded: https://testnet.xrpl.org/transactions/${cst_signed.hash}`)
+    } else {
+        throw `Error sending transaction: ${cst_result}`
+    }
+
+    // Configuring hot address settings by sending an ACCOUNT SET transaction
+    // to the XRP network
+    const hst_prepared = await client.autofill(hot_settings_tx)
+    const hst_signed = hot_wallet.sign(hst_prepared)
+
+    console.log("Sending hot address AccountSet transaction...")
+    const hst_result = await client.submitAndWait(hst_signed.tx_blob)
+
+    if (hst_result.result.meta.TransactionResult == "tesSUCCESS") {
+    console.log(`Transaction succeeded: https://testnet.xrpl.org/transactions/${hst_signed.hash}`)
+    } else {
+    throw `Error sending transaction: ${hst_result.result.meta.TransactionResult}`
+    }
+
+    // Creating trust line from hot to cold address
+    const ts_prepared = await client.autofill(trust_set_tx)
+    const ts_signed = hot_wallet.sign(ts_prepared)
+
+    console.log("Creating trust line from hot address to issuer...")
+    const ts_result = await client.submitAndWait(ts_signed.tx_blob)
+
+    if (ts_result.result.meta.TransactionResult == "tesSUCCESS") {
+        console.log(`Transaction succeeded: https://testnet.xrpl.org/transactions/${ts_signed.hash}`)
+    } else {
+        throw `Error sending transaction: ${ts_result.result.meta.TransactionResult}`
+    }
+
+    // Sending token from cold to hot address
+    const pay_prepared = await client.autofill(send_token_tx)
+    const pay_signed = cold_wallet.sign(pay_prepared)
+
+    console.log(`Sending ${issue_quantity} ${currency_code} to ${hot_wallet.address}...`)
+    const pay_result = await client.submitAndWait(pay_signed.tx_blob)
+
+    if (pay_result.result.meta.TransactionResult == "tesSUCCESS") {
+        console.log(`Transaction succeeded: https://testnet.xrpl.org/transactions/${pay_signed.hash}`)
+    } else {
+        throw `Error sending transaction: ${pay_result.result.meta.TransactionResult}`
+    }
+
+    // Check balances ------------------------------------------------------------
+    console.log("Getting hot address balances...")
+    const hot_balances = await client.request({
+        command: "account_lines",
+        account: hot_wallet.address,
+        ledger_index: "validated"
+    })
+    console.log(hot_balances.result)
+
+    console.log("Getting cold address balances...")
+    const cold_balances = await client.request({
+        command: "gateway_balances",
+        account: cold_wallet.address,
+        ledger_index: "validated",
+        hotwallet: [hot_wallet.address]
+    })
+    console.log(JSON.stringify(cold_balances.result, null, 2))
+
+    // Disconnect when done (If you omit this, Node.js won't end the process)
+    client.disconnect()
+}
+
+main()
---
+++ terminology.md
@@ -0,0 +1,25 @@
+COLD ADDRESS
+The cold address is the issuer of the token.
+
+HOT ADDRESS
+The hot address is like a regular user's address that you control.
+It receives tokens from the cold address, which you can then transfer to other users.
+
+Cold Address: rsB6zurmvcnsmMVyRB3A9BqZpug72A2Y38
+Cold Secret: ssmBMxNaemySaeBBQSPW33XdHjfiv
+XRP Balance: 1,000 XRP
+Hot Address: r9TatEuN31tLavVbtcB4oE82m1J36i8o8i
+Hot Secret: shTvPw9GDhb6rMQzMBGvSRvbJqzr1
+XRP Balance: 1,000 XRP
+
+ACCOUNT SET transaction
+When you add/change settings of of an address (cold or hot address), you need
+to report those changes to the network. That's what the ACCOUNT SET transaction
+is useful for
+
+TRUST LINE transaction
+Useful to create a trust line between two address. A hot address needs to establish
+a trust line with a cold address, so that it can start receiving tokens.
+
+PAYMENT transaction
+Used to send a token from cold to hot address
